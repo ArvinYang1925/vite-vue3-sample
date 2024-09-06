8b8405a3f3a2c8b316026c0cb184925ea722789e
ArvinYang1925 <arvinyang1925@gmail.com>
2024-09-06 16:14:34 +0800

    update code

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+// i. 定義值
+const num = ref(1) // 使用 ref 定義響應式的狀態 num，初始值為 1
+
+// ii. 針對值要做的事情
+function clickMe() {
+  num.value++
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  {{ num }}
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it! Well Done!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <button v-on:click="clickMe">Click</button>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
